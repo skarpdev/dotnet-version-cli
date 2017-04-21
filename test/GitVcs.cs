800f6618ac48494d3170feaa0b944ab97518d207
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-04-21 20:52:31 +0200

    Added a few tests of the GitVcs
    However they are Skipped per default as they depend on
    the local repository and thus cannot be trusted to work each
    time

---
+++ test/GitVcs.cs
@@ -0,0 +1,31 @@
+using Skarpdev.DotnetVersion.Vcs.Git;
+using Xunit;
+
+namespace Skarpdev.DotnetVersion.Test
+{
+    public class GitVcsTest
+    {
+        private GitVcs _vcs;
+
+        public GitVcsTest()
+        {
+            _vcs = new GitVcs();
+        }
+
+        [Fact(
+            Skip = "Dont run on build servers"
+        )]
+        public void DetectingGitOnMachineWorks()
+        {
+            Assert.True(_vcs.IsVcsToolPresent());
+        }
+
+        [Fact(
+            Skip = "Dont run on build servers"
+        )]
+        public void IsRepositoryCleanWorks()
+        {
+            Assert.True(_vcs.IsRepositoryClean());
+        }
+    }
+}
