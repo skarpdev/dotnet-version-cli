b257db02cc179738e8bd0abb855b9048c783633c
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-04-22 15:51:11 +0200

    Implement and test project version patcher

--- src/CsProj/ProjectFileVersionPatcher.cs
+++ src/CsProj/ProjectFileVersionPatcher.cs
@@ -1,17 +1,33 @@
 using System;
+using System.IO;
 
 namespace Skarpdev.DotnetVersion.CsProj
 {
     public class ProjectFileVersionPatcher
     {
-        internal void Patch(string v)
+        /// <summary>
+        /// Replace the existing version number in the csproj xml with the new version
+        /// </summary>
+        /// <param name="xml">The csproj xml loaded from disk</param>
+        /// <param name="oldVersion">The old version number present in the xml</param>
+        /// <param name="newVersion">The new version number to persist in the csproj file</param>
+        /// <returns></returns>
+        public string Patch(string xml, string oldVersion, string newVersion)
         {
-            throw new NotImplementedException();
+            return xml.Replace(
+                $"<Version>{oldVersion}</Version>", 
+                $"<Version>{newVersion}</Version>"
+            );
         }
 
-        internal void Flush(string resolveCsProjFile)
+        /// <summary>
+        /// Save the csproj changes to disk
+        /// </summary>
+        /// <param name="xml">The csproj xml content</param>
+        /// <param name="filePath">The path of the csproj to write to</param>
+        public void Flush(string xml, string filePath)
         {
-            throw new NotImplementedException();
+            File.WriteAllText(filePath, xml);
         }
     }
 }
---
+++ test/CsProj/ProjectFileVersionPatcherTest.cs
@@ -0,0 +1,34 @@
+using System.Text.RegularExpressions;
+using Skarpdev.DotnetVersion.CsProj;
+using Xunit;
+
+namespace Skarpdev.DotnetVersion.Test.CsProj
+{
+    public class ProjectFileVersionPatcherTest
+    {
+        private static string _projectXml =
+                    "<Project Sdk=\"Microsoft.NET.Sdk\">" +
+                    "<PropertyGroup>" +
+                    "<TargetFramework>netstandard1.6</TargetFramework>" +
+                    "<RootNamespace>Unit.For.The.Win</RootNamespace>" +
+                    "<PackageId>Unit.Testing.Library</PackageId>" +
+                    "<Version>1.0.0</Version>" +
+                    "</PropertyGroup>" +
+                    "</Project>";
+        private ProjectFileVersionPatcher _patcher;
+
+        public ProjectFileVersionPatcherTest()
+        {
+            _patcher = new ProjectFileVersionPatcher();
+        }
+
+        [Fact]
+        public void CanPatchVersionOnWellFormedXml()
+        {
+            var newXml = _patcher.Patch(_projectXml, "1.0.0", "1.1.0");
+
+            Assert.NotEqual(_projectXml, newXml);
+            Assert.True(newXml.Contains("<Version>1.1.0</Version>"));
+        }
+    }
+}
