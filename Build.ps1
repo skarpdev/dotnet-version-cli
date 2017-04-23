97edbb9697ee1a2099b38684eab02a6361a29041
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-04-23 11:53:45 +0200

    Add appveyor.yml file
    For now configuring builds on master to go to myget
    Commented code for pushing to nuget on tags as we need it later

--- Build.ps1
+++ Build.ps1
@@ -29,6 +29,8 @@ exec { & dotnet restore }
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
+exec { & dotnet build }
+
 exec { & dotnet test .\test\dotnet-version-test.csproj -c Release }
 
 exec { & dotnet pack .\src\dotnet-version.csproj -c Release -o .\artifacts --include-source }
