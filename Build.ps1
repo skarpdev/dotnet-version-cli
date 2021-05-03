05286718d22b4f314309f2e3f6b3da9cb28b1f17
Nicklas Laine Overgaard <nicklas@skarp.dk>
2021-05-03 19:28:28 +0200

    Change default java in Build.ps1 script

--- Build.ps1
+++ Build.ps1
@@ -24,6 +24,9 @@ function Exec
 
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
+$Env:JAVA_HOME="C:\Program Files\Java\jdk11"
+$Env:PATH=-join($Env:JAVA_HOME, ";", $Env:PATH)
+
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
